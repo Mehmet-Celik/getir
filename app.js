6d94a968ef0381cd3cf2e6a70479dd2ab7a87ce7
DESKTOP-D20EINA\Mehmet <1mehmethuseyincelik@gmail.com>
2019-07-10 22:09:33 +0300

    port fixed

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 // eslint-disable-next-line no-undef
-const port = process.env.PATH;
+const port = process.env.PORT;
 
 app.get("/", (req, res) => {
     res.json({ "code": 0, "msg": "Success", "records": [{ "key": "TAKwGc6Jr4i8Z487", "createdAt": "2017-01-28T01:22:14.398Z", "totalCount": 2800 }, { "key": "NAeQ8eX7e5TEg7oH", "createdAt": "2017-01-27T08:19:14.135Z", "totalCount": 2900 }] }
